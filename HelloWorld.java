1003cc9b9aa0db95571ac8a7da0436c5d891f4db
anandven1975 <124437028+anandven1975@users.noreply.github.com>
2023-02-05 03:22:51 +1100

    Add files via upload
    Week1 . Day 1 Assignments

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int i= 100;
+		boolean b=true;
+		String name="Anand";
+		
+		System.out.println(name);
+		System.out.println(i);
+		System.out.println(b);	
+		
+			
+
+	}
+
+}
---
+++ Mobile.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mobileBrandName="Samsung";
+		char mobileLogo='S';	
+		short noOfMobilePiece=50;
+		int modelNumber=30;
+		long mobileImeiNumber=123543566L;
+		float mobilePrice=3454.23f;
+		boolean isDamaged=false	;
+		
+		//Print values
+		System.out.println("Mobile Brand Name : "+mobileBrandName);
+		System.out.println("Mobile Logo       : "+mobileLogo);
+		System.out.println("Mobile Piece      : "+noOfMobilePiece);
+		System.out.println("Model Number      : "+modelNumber);
+		System.out.println("Mobile IMEI Number: "+mobileImeiNumber);
+		System.out.println("Mobile Price      : "+mobilePrice);
+		System.out.println("Damaged           : "+isDamaged);
+		
+	}
+
+}
